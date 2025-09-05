ab1b48d01cb424d93483a5027697c04ecd2774b5
Андрей Бускин <Andreyya1996@gmail.com>
2025-09-05 18:40:46 +0400

    Добавьте файлы проекта.

---
+++ Resto.Front.Api.DataSaturation/DataSaturationPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Resto.Front.Api.Attributes;
+using Resto.Front.Api.Attributes.JetBrains;
+
+namespace Resto.Front.Api.DataSaturation
+{
+    [UsedImplicitly, PluginLicenseModuleId(ModuleId)]
+    public class DataSaturationPlugin : IFrontPlugin
+    {
+        private const int ModuleId = 21016318;
+        public DataSaturationPlugin() 
+        {
+            
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Resto.Front.Api.DataSaturation/Services/ProductsService.cs
@@ -0,0 +1,23 @@
+﻿using Resto.Front.Api.Data.Assortment;
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+
+namespace Resto.Front.Api.DataSaturation.Services
+{
+    public class ProductsService
+    {
+        private readonly CompositeDisposable subscriptions = new CompositeDisposable();
+        public ProductsService() 
+        {
+            subscriptions.Add(PluginContext.Notifications.ProductChanged.Subscribe(ProductChanged));
+        }
+
+        private void ProductChanged(IProduct product)
+        {
+            var barcode = product.BarcodeContainers.FirstOrDefault();
+            PluginContext.Log.Info($"[{nameof(ProductsService)}|{nameof(ProductChanged)}] Get changed product id = {product.Id} code = {barcode} price = {product.Price}");
+
+        }
+    }
+}
